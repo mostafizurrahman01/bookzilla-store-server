b1176b33bc96662432524300c23001c0983e7910
mostafizurrahman01 <abirmostafizurrahman@gmail.com>
2021-04-02 23:48:46 +0600

    All

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rbbe2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
